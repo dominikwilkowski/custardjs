311a6238dfcf242f583c0231a908cf29945005a3
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2016-05-10 15:45:14 +1000

    added first working version
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- README.md
+++ README.md
@@ -12,37 +12,86 @@
 
 > A simple priority system for NodeJS
 
-## Examples
+## Why
 
-//TODO
+When running a node application and your boss wants you to add funcky little things that add no value but bring your applications performance down you want to
+start sacrificing those functions when the demand gets too high.
+CustradJS simply keeps track of how many requests are running and starts sacrificing sub functions you specify according to a `maxCalls` property you can set.
+
 
 ## Installing
 
-To install the CLI app, simply NPM install it globally.
+To install simply NPM install it and require it in your project.
 
 ```shell
 $ npm install custardjs
 ```
 
-To use it in your project:
-
 ```js
-//TODO
+var Custard = require('../prod/dev.js');
+
+Custard.run([
+	{
+		'run': function1,
+	},{
+		'run': function2,
+		'maxCalls': 100,
+	},{
+		'run': function3,
+		'maxCalls': 900,
+	}
+]);
 ```
 
 
 ## Usage
 
-//TODO
+#### run
+Type: `<method>`
+Parameters: `targets <object>` and `failsafe <function> (optional)`
 
-#### -m, --max-length //TODO
-Type: `<integer>` //TODO
-Default value: `10` //TODO
+The main method to invoke all parts of your application and assigned priority. `maxCalls` means the method or function will only be run if there are less then
+`maxCalls` processes running at the same time.
+The failsafe function is optional and is run when we drop runs becasue we reached `maxCalls` capacity.
 
-//TODO
+```js
+Custard.run([
+	{
+		'run': function1, //run this always
+	},{
+		'run': function2,
+		'maxCalls': 100, //only run this if there are less than 100 processes currently running
+	},{
+		'run': function3,
+		'maxCalls': 900, //only run this if there are less than 900 processes currently running
+	}
+], function() { //run this when we start sacrificing some of our functionality
+	console.log('Stuff we can do to report when things start to crack down!');
 
-```shell
-//TODO
+	//send email to sys-admin
+	//automatically scale system
+	//panik and run against a wall
+});
+```
+
+#### finished
+Type: `<method>`
+Parameters: `none`
+
+When all your functions completed and your promise is resloved, run this method to make sure all calls are counted correctly.
+
+```js
+Custard.finished();
+```
+
+#### get
+Type: `<method>`
+Parameters: `none`
+
+If you want to know how many calls are in the queue mid-flow, run the `get` method. It returns the number of calls in queue.
+
+```js
+Custard.get()
 ```
 
 
@@ -51,6 +100,7 @@ Please look at the coding style and work with it, not against it ;)
 
 
 ## Release History
+* 0.1.0  -  First working example
 * 0.0.1  -  initial commit
 
 
--- dev/010-app.js
+++ dev/010-app.js
@@ -9,6 +9,7 @@
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Dependencies
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------
+var Chalk = require('chalk');
 var Fs = require('fs');
 
 
@@ -19,64 +20,119 @@ var Custard = (function Application() {
 		// Settings
 		//----------------------------------------------------------------------------------------------------------------------------------------------------------
 		QUEUE: 0,
+		DEBUG: [Debug],
 
 		//----------------------------------------------------------------------------------------------------------------------------------------------------------
 		// Running a new request
 		//
 		// @param  targets   [function]   The function to be run
+		// @param  failsafe  [function]   A function to be executed when bits start to be sacrificed
 		//
 		// @return           [string]     ID of the function run
 		//----------------------------------------------------------------------------------------------------------------------------------------------------------
-		run: function Running( targets ) {
+		run: function Running( targets, failsafe ) {
 			Custard.QUEUE ++;
 
 			targets.forEach(function iterateArguments( bit ) {
-				console.log('iteration');
 				//error handling
 				if( typeof bit.run !== 'function' ) {
-					console.log('Error dude!: ' + typeof bit.run);
+					Custard.debugging( 'The run bit of your sequenze must be a function! It is instead: ' + typeof bit.run, 'error' );
 
 					return;
 				}
 
-				if( typeof bit.priority !== 'number' ) {
-					if( typeof bit.priority !== 'undefined' ) {
-						bit.priority = 0;
+				if( typeof bit.maxCalls !== 'number' ) {
+					if( typeof bit.maxCalls === 'undefined' ) {
+						bit.maxCalls = 0; //set default to 0 (always run)
 					}
 					else {
-						console.log('Error dude!: ' + typeof bit.priority);
+						Custard.debugging( 'The maxCalls bit of your sequenze must be an integer! It is instead: ' + typeof bit.maxCalls, 'error' );
+
 						return;
 					}
 				}
 
-				var myObjAsync = promisify( bit.run );
+				//check the maxCalls
+				if( Custard.QUEUE < bit.maxCalls || bit.maxCalls === 0 ) {
+					Custard.debugging( 'Calling ' + bit.run.name + ' and a QUEUE: ' + Custard.QUEUE, 'report' );
 
-				myObjAsync().then(function(data) {
-					console.log("%j", data);
-					console.log('???');
-					return data;
-				});
+					bit.run(); //run what needs to be run!
+				}
+				else {
+					Custard.debugging( 'STUFF IS BEING SACRIFICED!!!', 'failsafe' );
 
-				// PW( bit.run ).then(function( data ) {
-				// 	console.log('done');
-				// }).error(function( err ) {
-				// 	console.log('ERROR!');
-				// });
-
-				console.log('end!');
+					if( typeof failsafe === 'function' ) {
+						failsafe(); //run failsafe if specified
+					}
+				}
 			});
 		},
 
 
 		//----------------------------------------------------------------------------------------------------------------------------------------------------------
 		// Finishing a queue element
+		//
+		// @return  [integer]  Amount of calls still pending
 		//----------------------------------------------------------------------------------------------------------------------------------------------------------
-		finish: function Finishing() {
+		finished: function Finishing() {
 			Custard.QUEUE --;
 
-			var ID = title;
+			Custard.debugging('Current queue: ' + Custard.QUEUE);
 
-			console.log( 'End! ' + ID );
+			if( Custard.QUEUE < 1 ) {
+				Custard.debugging( 'I got no other tasks running', 'finished' );
+			}
+
+			return Custard.get();
+		},
+
+
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------
+		// Getting how many calls are in progress
+		//
+		// @return  [integer]  Amount of calls
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------
+		get: function getting() {
+			return Custard.QUEUE;
+		},
+
+
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------
+		// Debugging prettiness
+		//
+		// @param  text  [string]   Text to be printed to debugger
+		// @param  code  [string]   The urgency as a string: ['report', 'error', 'interaction', 'send', 'receive']
+		//
+		// @return       [ansi]     console.log output
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------
+		debugging: function Debugging( text, code ) {
+
+			if( code === 'headline' ) {
+				if( Custard.DEBUG ) {
+					var fonts = new CFonts({
+						'text': text,
+						'colors': ['white', 'gray'],
+						'maxLength': 12,
+					});
+				}
+			}
+
+			if( code === 'report' ) {
+				if( Custard.DEBUG ) console.log(Chalk.bgWhite("\n" + Chalk.bold.green(' \u2611  ') + Chalk.black( 'Custard: ' + text + ' ')));
+			}
+
+			else if( code === 'error' ) {
+				console.log(Chalk.bgWhite("\n" + Chalk.red(' \u2612  ') + Chalk.black( 'Custard: ' + text + ' ')));
+			}
+
+			else if( code === 'finished' ) {
+				if( Custard.DEBUG ) console.log(Chalk.bgGreen("\n" + Chalk.bold.cyan(' \u219C  ') + Chalk.black( 'Custard: ' + text + ' ')));
+			}
+
+			else if( code === 'failsafe' ) {
+				if( Custard.DEBUG ) console.log(Chalk.bgYellow("\n" + Chalk.bold.cyan(' \u2612  ') + Chalk.black( 'Custard: ' + text + ' ')));
+			}
+
 		},
 	};
 }());
